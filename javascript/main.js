969194d81ab5785f8e939d22fe52693ab9ba3e0c
Vicky Clitheroe <vickyclitheroe@gmail.com>
2015-06-10 21:59:30 +0100

    added alert to mouseenter

--- javascript/main.js
+++ javascript/main.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
 
-   alert("I am an alert box!");
+   
 
 
 
 $('li').mouseenter(function() {
-    $(this).effect('bounce',500);
-});​
+    alert("I am an alert box!");
 
+});
 });
