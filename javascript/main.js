b35b3639410229168207903976f7c64cce2649e9
Vicky Clitheroe <vickyclitheroe@gmail.com>
2015-07-08 14:23:05 +0100

    moved setting up of queryloader back into index.html

--- javascript/main.js
+++ javascript/main.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
 
-    $("body").queryLoader2(   
-    		 barColor: "#efefef",
-             backgroundColor: "#111",
-             percentage: true,
-             barHeight: 1,
-             completeAnimation : "fade",
-             minimumTime: 200,
-             fadeOutTime: 1000);
+//    $("body").queryLoader2(   
+  //  		 barColor: "#efefef",
+    //         backgroundColor: "#111",
+      //       percentage: true,
+        //     barHeight: 1,
+          //   completeAnimation : "fade",
+            // minimumTime: 200,
+            // fadeOutTime: 1000);
 });
