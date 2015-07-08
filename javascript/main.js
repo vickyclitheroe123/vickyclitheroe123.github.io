92d46786acdfce51fa27d932b3147d2bd563603f
Vicky Clitheroe <vickyclitheroe@gmail.com>
2015-07-08 14:09:35 +0100

    setup page loader in jquery on ready

---
+++ javascript/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $("body").queryLoader2(   
+    		 barColor: "#efefef",
+             backgroundColor: "#111",
+             percentage: true,
+             barHeight: 1,
+             completeAnimation : "fade",
+             minimumTime: 20000,
+             fadeOutTime: 1000);
+});
